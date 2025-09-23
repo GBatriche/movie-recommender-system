46c3ee38a651a1d4db4891da0500efd8ccab2e9b
GBatriche <batricheg@gmail.com>
2025-09-23 11:47:02 -0500

    feat: modificando a função recommendMovies(), criando um objeto da classe CollaborativeFilter em vez de ContentBasedFilter

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson1/RecommenderImplementation.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson1/RecommenderImplementation.java
@@ -2,8 +2,9 @@ package io.datajek.spring.basics.movie_recommender_system.lesson1;
 
 public class RecommenderImplementation {
     public String[] recommendMovies(String movie) {
-        ContentBasedFilter filter = new ContentBasedFilter();
-        String[] results = filter.getRecommendations(movie);
+        CollaborativeFilter filter = new CollaborativeFilter();
+        String[] results =
+                filter.getRecommendations("Finding Dory");
         return results;
     }
 }
