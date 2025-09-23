5a93e9b28925353d9e996c2313434f6e5f6c41a8
GBatriche <batricheg@gmail.com>
2025-09-23 15:04:44 -0500

    feat: adicionar o @Primary para o CollaborativeFilter

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/logic/CollaborativeFilter.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/logic/CollaborativeFilter.java
@@ -1,5 +1,10 @@
 package io.datajek.spring.basics.movie_recommender_system.logic;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class CollaborativeFilter implements Filter {
 
     public String[] getRecommendations(String movie) {
