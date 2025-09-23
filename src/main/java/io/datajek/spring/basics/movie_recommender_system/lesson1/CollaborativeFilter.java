4a378e5494cf17d596351b423005ef2b261cb625
GBatriche <batricheg@gmail.com>
2025-09-23 11:44:12 -0500

    feat: criando a classe CollaborativeFilter

---
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson1/CollaborativeFilter.java
@@ -0,0 +1,7 @@
+package io.datajek.spring.basics.movie_recommender_system.lesson1;
+
+public class CollaborativeFilter {
+    public String[] getRecommendations(String movie) {
+        return new String[] {"Finding Nemo", "Ice Age", "Toy Story"};
+    }
+}
