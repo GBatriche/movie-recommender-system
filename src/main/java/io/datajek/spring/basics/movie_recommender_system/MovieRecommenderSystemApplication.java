c7ec3f731798c9436d76d40e0c0d0581ad69fac2
GBatriche <batricheg@gmail.com>
2025-09-23 14:58:21 -0500

    feat: adicionar o application context

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/MovieRecommenderSystemApplication.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/MovieRecommenderSystemApplication.java
@@ -3,15 +3,17 @@ import java.util.Arrays;
 
 import io.datajek.spring.basics.movie_recommender_system.logic.ContentBasedFilter;
 import io.datajek.spring.basics.movie_recommender_system.logic.RecommenderImplementation;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
     public static void main(String[] args) {
-        RecommenderImplementation recommender = new RecommenderImplementation(new ContentBasedFilter());
+        ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+        RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
         String[] result = recommender.recommendMovies("Finding Dory");
         System.out.println(Arrays.toString(result));
-
     }
 }
