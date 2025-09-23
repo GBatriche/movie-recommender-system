9d8f0082dfea4bd97a57a46cd07f39ba244f4f82
GBatriche <batricheg@gmail.com>
2025-09-23 11:54:35 -0500

    feat: RecommenderImplementation usando a interface

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson1/RecommenderImplementation.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson1/RecommenderImplementation.java
@@ -1,10 +1,16 @@
 package io.datajek.spring.basics.movie_recommender_system.lesson1;
 
 public class RecommenderImplementation {
-    public String[] recommendMovies(String movie) {
-        CollaborativeFilter filter = new CollaborativeFilter();
-        String[] results =
-                filter.getRecommendations("Finding Dory");
+    private Filter filter;
+
+    public RecommenderImplementation() {
+        super();
+        this.filter = filter;
+    }
+    public String [] recommendMovies (String movie) {
+        //print the name of interface implementation being used
+        System.out.println("Name of the filter in use: " + filter + "\n");
+        String[] results = filter.getRecommendations("Finding Dory");
         return results;
     }
-}
+    }
